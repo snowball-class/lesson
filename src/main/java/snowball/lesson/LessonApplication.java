db9685fbd5741b93db7616a3b37326208ff21da7
Chanui <jcu011@naver.com>
2025-02-27 16:19:57 +0900

    feat: feignclient 추가

--- src/main/java/snowball/lesson/LessonApplication.java
+++ src/main/java/snowball/lesson/LessonApplication.java
@@ -1,7 +1,10 @@
 package snowball.lesson;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class LessonApplication {
 
